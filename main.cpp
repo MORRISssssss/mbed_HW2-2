3779747097aff1435e1e8593f73ad8617bc2adc7
MorisZheng <s04044mort@gmail.com>
2023-03-25 14:28:47 +0800

    up

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@ AnalogOut aout(PA_4);
 
 Thread LEDThread; 
 Thread sensorThread;
-float sample[400];
+float sample[100];
 
 void pwmLED()
 {
     int cnt = 0;
     while (true)
     {
-        LED.pulsewidth_us(4000 * sample[cnt]);
+        LED.pulsewidth_us(5000 * sample[cnt]);
         cnt++;
-        if (cnt == 400) cnt = 0;
+        if (cnt == 100) cnt = 0;
         ThisThread::sleep_for(10ms);
     }
 }
@@ -30,17 +30,17 @@ void sensor()
 
 int main()
 {
-    for (int i = 0; i < 400; i++){
-        if (i < 100)
-            sample[i] = (float)i / 100;
-        else if (i < 200)
+    for (int i = 0; i < 100; i++){
+        if (i < 25)
+            sample[i] = (float)i / 25;
+        else if (i < 50)
             sample[i] = 1.;
-        else if (i < 300)
-            sample[i] = 1. - (i - 200.) / 100;
+        else if (i < 75)
+            sample[i] = 1. - (i - 50.) / 25;
         else
             sample[i] = 0;
     }
-    LED.period_ms(4);
+    LED.period_ms(5);
     LEDThread.start(pwmLED);
     sensorThread.start(sensor);
     while (true){
